df40842c4c4c6c8fb524a897c0c9bbbb5704092e
Madeline Jimenez <madelineijimenez@gmail.com>
2020-04-10 21:06:46 -0400

    Create get save delete functions in API

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -32,4 +32,16 @@ export default {
             .catch(err => reject(err));
         });
     },
+    // Gets all books
+    getBooks: function() {
+        return axios.get("/api/books");
+    },
+    // Deletes the book with the given id
+    deleteBook: function(id) {
+        return axios.delete("/api/books/" + id);
+    },
+    // Saves a book to the database
+    saveBook: function(bookData) {
+        return axios.post("/api/books", bookData);
+    }
 };
